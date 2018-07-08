4dc17154dfcdd018321848cfd5a490559c53458f
Fabrizio De Sanctis <36366729+fabrizio-desanctis@users.noreply.github.com>
2018-07-08 22:47:19 +0200

    Creata interfaccia DAO Evento

---
+++ Code/EM17_Desktop/src/dao/concrete/oracle/EventoOracleDAO.java
@@ -0,0 +1,5 @@
+package dao.concrete.oracle;
+
+public class EventoOracleDAO {
+
+}
---
+++ Code/EM17_Desktop/src/dao/interfaces/EventoDAO.java
@@ -0,0 +1,10 @@
+package dao.interfaces;
+
+import models.Evento;
+import java.util.LinkedList;
+
+public interface EventoDAO {
+
+	public LinkedList<Evento> getListaEventi ();
+	
+}
