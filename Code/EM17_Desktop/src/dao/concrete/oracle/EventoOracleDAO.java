10e785a99e114465e5b9e431f57eb66d41c70153
Fabrizio De Sanctis <36366729+fabrizio-desanctis@users.noreply.github.com>
2018-07-09 10:39:59 +0200

    Aggiornato Evento

--- Code/EM17_Desktop/src/dao/concrete/oracle/EventoOracleDAO.java
+++ Code/EM17_Desktop/src/dao/concrete/oracle/EventoOracleDAO.java
@@ -1,5 +1,13 @@
 package dao.concrete.oracle;
 
-public class EventoOracleDAO {
+import dao.interfaces.EventoDAO;
+import models.Evento;
+import java.util.LinkedList;
 
+public class EventoOracleDAO implements EventoDAO{
+	
+@Override
+public LinkedList<Evento> getListaEventi () {
+	return new LinkedList<Evento> ();
+}
 }
